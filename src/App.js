7d6253b6322f091a969930a42e4020bfe156221b
Luiz Ricardo Perovano <ricardoperovano@gmail.com>
2021-01-26 21:59:48 -0300

    set up

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import Link from "./components/link";
 import Menu from "./components/menu";
 import "./App.css";
 import "semantic-ui-css/semantic.min.css";
+import AuthorizedRoute from "./components/AuthorizedRoute";
+import GuestRoute from "./components/GuestRoute";
+import Login from "./components/login";
+import Create from "./components/create";
+import { Route } from "react-router-dom";
 
-function App() {
+const App = ({ location }) => {
   return (
     <div className="container">
       <Menu />
-      <Link />
+      <AuthorizedRoute location={location} path="/" exact component={Link} />
+      <GuestRoute location={location} path="/login" exact component={Login} />
+      <Route location={location} path="/create" exact component={Create} />
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Route component={App} />
+  </BrowserRouter>,
   document.getElementById("root")
 );
